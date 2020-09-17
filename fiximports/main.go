2c42f470c603adecfa1a82695d01c20da0bbd952
Corey Scott <corsc@users.noreply.github.com>
2020-09-17 10:45:49 +1000

    [All] Quickly add Go Mod support and vendor deps

--- fiximports/main.go
+++ fiximports/main.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Corey Scott http://www.sage42.org/
+// Copyright 2017- Corey Scott http://www.sage42.org/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
